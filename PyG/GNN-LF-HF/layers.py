4ff59cf2c4da40b249229f2c37b993a1cf39eb11
倚栏听风 <1845706088@qq.com>
2022-10-19 22:17:10 +0800

    modify code

--- PyG/GNN-LF-HF/layers.py
+++ PyG/GNN-LF-HF/layers.py
@@ -1,6 +1,7 @@
 import torch
-import torch_geometric.utils as utils
 from torch_geometric.nn import MessagePassing
+
+from util import norm_Adj
 
 
 class GNNHFConv(MessagePassing):
@@ -11,12 +12,7 @@ class GNNHFConv(MessagePassing):
 
     def forward(self, x, edge_index):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        edge_index, _ = utils.add_self_loops(edge_index, num_nodes=x.shape[0])
-        adj = utils.to_scipy_sparse_matrix(edge_index).todense()
-        adj = torch.Tensor(adj).to(device)
-        row, col = edge_index[0], edge_index[1]
-        deg_inv_sqrt = norm(col, x.size(0), x.dtype)
-        adj = torch.mm(torch.mm(deg_inv_sqrt, adj), deg_inv_sqrt)
+        adj = norm_Adj(x, edge_index, device)
         I = torch.eye(x.shape[0]).to(device)
         pre = (self.beta + 1 / self.alpha) * I + (1 - self.beta - 1 / self.alpha) * adj
         res = torch.mm((I + self.beta * adj), x)
@@ -31,21 +27,8 @@ class GNNLFConv(MessagePassing):
 
     def forward(self, x, edge_index):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        edge_index, _ = utils.add_self_loops(edge_index, num_nodes=x.shape[0])
-        adj = utils.to_scipy_sparse_matrix(edge_index).todense()
-        adj = torch.Tensor(adj).to(device)
-        row, col = edge_index[0], edge_index[1]
-        deg_inv_sqrt = norm(col, x.size(0), x.dtype)
-        adj = torch.mm(torch.mm(deg_inv_sqrt, adj), deg_inv_sqrt)
+        adj = norm_Adj(x, edge_index, device)
         I = torch.eye(x.shape[0]).to(device)
         pre = (self.mu + 1 / self.alpha - 1) * I + (2 - self.mu - 1 / self.alpha) * adj
         res = torch.mm(self.mu * I + (1 - self.mu) * adj, x)
         return torch.matmul(torch.linalg.inv(pre), res)
-
-
-def norm(col, size, type):
-    deg = utils.degree(col, size, dtype=type)
-    deg_inv_sqrt = deg.pow_(-0.5)
-    deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0)
-    deg_inv_sqrt = torch.diag_embed(deg_inv_sqrt)
-    return deg_inv_sqrt
